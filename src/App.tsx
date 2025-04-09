21caffceb846c4b7ebc01a9cb8dd96b18b7a04cf
Jason <wangsanmu5@163.com>
2025-04-09 20:37:35 +0800

    feat: init vite+shadcn

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Click me111</Button>
+        <Input type="text" />
+      </div>
+    </>
+  )
+}
+
+export default App
